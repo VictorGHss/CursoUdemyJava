803bf32e1245dd4bda5158b836ed144adf16fbc9
Viteykk <vg.hss0101@gmail.com>
2024-12-28 11:32:55 -0300

    primeiro commit

---
+++ fundamentos/Inferencia.java
@@ -0,0 +1,22 @@
+package fundamentos;
+
+public class Inferencia {
+
+	public static void main(String[] args) {
+
+		double a = 4.5;
+		System.out.println(a);
+
+		var b = 2.5;
+		System.out.println(b);
+
+		var c = "texto";
+		System.out.println(c);
+
+		double d; // variavel declarada
+		d = 123.45; // variavel inicializada
+		System.out.println(d); // variavel usada
+
+	}
+
+}
---
+++ fundamentos/TiposPrimitivos.java
@@ -0,0 +1,33 @@
+package fundamentos;
+
+public class TiposPrimitivos {
+	
+	public static void main(String[] args) {
+		// info do funcionario
+		
+		// tipos numericos inteiros
+		byte anosdeEmpresa = 23;
+		short numeroDeVoos = 542;
+		int id = 56731;
+		long pontosAcumulados = 3_234_783_154L;
+		
+		// tipos numericos reais
+		
+		float salario = 11_555.77F;
+		double vendasAcumuladas = 2_991_70797_103.01;
+		
+		//tipo booleano
+		
+		boolean estaDeFerias = false;
+	  //boolean estaDeFerias = true;
+		
+		//tipo caractere
+		
+		char status = 'A';
+		
+		//dias de empresa
+		
+		
+	}
+
+}
